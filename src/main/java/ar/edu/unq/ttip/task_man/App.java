c42f774a6edd32a9b92c5eb915c4bae3d629869a
leandro <diazleandro4@gmail.com>
2019-09-06 00:59:28 -0300

    preparando entorno de trabajo

---
+++ src/main/java/ar/edu/unq/ttip/task_man/App.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.ttip.task_man;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
